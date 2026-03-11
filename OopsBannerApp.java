e0bec0b0298f18d2c2b10440ddf6a225c66ab9b2
Vatsal <vatsal.mundra2511@gmail.com>
2026-03-11 09:31:57 +0530

    UC2 Display OOPS in Banner Format

---
+++ OopsBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * OOPS Banner App - Use Case 2
+ * Transitioning to a 7-line ASCII art display using asterisks and spaces.
+ * @author Vatsal Mundra
+ * @version 1.0
+ */
+public class OopsBannerApp {
+    public static void main(String[] args) {
+        // UC2: Displaying OOPS in a Banner Format using individual print statements
+        System.out.println("  *** *** **** **** ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * **** **** ");
+        System.out.println(" * * * * * *");
+        System.out.println("  *** *** * **** ");
+        System.out.println(); // Optional blank line for spacing
+    }
+}
